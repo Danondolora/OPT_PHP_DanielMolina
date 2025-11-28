023464137c9028a7756e89d3ff7a911b79ff3886
Danondolora <danielmolinasanchez01@gmail.com>
2025-11-28 10:16:35 +0100

    Add files via upload

--- formulario.php
+++ formulario.php
@@ -1,30 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php
-        $prov_sede = [
-            "Málaga"  => ["Benalmádena", "Málaga"],
-            "Sevilla" => ["Dos Hermanas", "Alcalá de Guadaíra"],
-            "Valencia" => ["Gandía", "Torrent"],
-            "Madrid"  => ["Móstoles", "Alcalá de Henares"],
-            "Barcelona" => ["Hospitalet de Llobregat", "Badalona"],
-            "Granada" => ["Motril", "Almuñécar"]
-        ];
-        $json_provincias = json_encode($prov_sede);
-
-        $departs = [
-        "Dirección General",
-        "Finanzas / Contabilidad",
-        "Recursos Humanos",
-        "Tecnologías de la Información / Sistemas",
-        "Desarrollo de Software / Ingeniería",
-        "Seguridad de la Información",
-        "Comercial / Ventas",
-        "Marketing",
-        "Atención al Cliente / Soporte",
-        "Producción / Fabricación",
-        "Logística / Cadena de Suministro",
-        "Calidad"
-        ];
+        require_once "datos.php";
     ?>
 
     <body>
